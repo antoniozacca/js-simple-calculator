dbac0f3b1c321a2e0b24dbc85fb314020a8fdb98
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-09 17:50:47 +0200

    inizio operatori

--- calculator/assets/main.js
+++ calculator/assets/main.js
@@ -1,10 +1,21 @@
 const numbers = document.querySelectorAll(".numeri");
+const operatori = document.querySelectorAll(".operatori");
+const operatoreSomma = document.querySelector(".operatore-somma");
+const operatoreUguale = document.querySelector(".operatore-uguale")
+
+
 
 for (let i = 0; i < numbers.length; i++) {
     const pulsanti = numbers[i];
     pulsanti.addEventListener('click', function () {
         const result = document.getElementById("result");
-        result.innerText = i;
+        result.innerText = pulsanti.innerHTML;
+        
+        for (let i = 0; i < operatori.length; i++) {
+            operatori[i].addEventListener('click', function() {
+              
+            });
+          }
     })
     
 }
