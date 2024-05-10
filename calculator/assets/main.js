38fc20005722ca1293256e725e1695a9715d043a
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-10 14:25:10 +0200

    fix cancella

--- calculator/assets/main.js
+++ calculator/assets/main.js
@@ -1,21 +1,41 @@
 const numbers = document.querySelectorAll(".numeri");
 const operatori = document.querySelectorAll(".operatori");
-const operatoreSomma = document.querySelector(".operatore-somma");
-const operatoreUguale = document.querySelector(".operatore-uguale")
+const somma = document.getElementById("operatore-addizione");
+const sottrazione = document.getElementById("operatore-sottrazione");
+const moltiplicazione = document.getElementById("operatore-moltiplicazione");
+const divisione = document.getElementById("operatore-divisione");
+const uguale = document.getElementById("operatore-uguale");
+const cancella = document.getElementById("operatore-canc");
 
-
-
+let result = document.getElementById("result");
 for (let i = 0; i < numbers.length; i++) {
     const pulsanti = numbers[i];
     pulsanti.addEventListener('click', function () {
-        const result = document.getElementById("result");
-        result.innerText = pulsanti.innerHTML;
-        
-        for (let i = 0; i < operatori.length; i++) {
-            operatori[i].addEventListener('click', function() {
-              
-            });
-          }
+        result.innerText += pulsanti.innerHTML;
     })
     
 }
+let risultato;
+for (let i = 0; i < operatori.length; i++) {
+    operatori[i].addEventListener('click', function() {
+        
+      switch (operatori[i].id) {
+        case "operatore-addizione":
+            risultato = "operatore-addizione";
+      
+        case "operatore-sottrazione":
+            risultato = "operatore-sottrazione";
+            break;
+        case "operatore-moltiplicazione":
+            risultato = "operatore-moltiplicazione";
+            break;
+        case "operatore-divisione":
+            risultato = "operatore-divisione";
+            break;
+      }
+    });
+  }
+
+  cancella.addEventListener('click', function(){
+    result.innerHTML= ' ';
+  })
